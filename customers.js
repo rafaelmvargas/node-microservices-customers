9144b05ce1e9abecfbcbad6fd92f0e08af66bc3b
Rafael Vargas <rafael.vargas@outlook.com>
2022-04-15 00:10:35 -0400

    load model, create post method

--- customers.js
+++ customers.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/customer', () => {
+  console.log('Connected to Customer DB');
+});
+
+// Load out model
+require('./Customer');
+const Customer = mongoose.model('Customer');
+
+app.post('/customer', (req, res) => {
+  var newCustomer = {
+    name: req.body.name,
+    age: req.body.age,
+    address: req.body.address,
+  };
+
+  var customer = new Customer(newCustomer);
+
+  customer.save().then(() => {
+    res.send('Customer created').catch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+});
 
 app.listen('55555', () => {
   console.log('Up and running - Customer service');
