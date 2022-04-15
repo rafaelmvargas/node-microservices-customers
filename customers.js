2762730f62efbfa55af3fc5287a931c224cad1e2
Rafael Vargas <rafael.vargas@outlook.com>
2022-04-15 01:00:07 -0400

    add get customers/customer, add delete

--- customers.js
+++ customers.js
@@ -31,6 +31,46 @@ app.post('/customer', (req, res) => {
   });
 });
 
+app.get('/customers', (req, res) => {
+  Customer.find()
+    .then((customers) => {
+      res.json(customers);
+    })
+    .catch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+});
+
+app.get('/customer/:id', (req, res) => {
+  Customer.findById(req.params.id)
+    .then((customer) => {
+      if (customer) {
+        res.json(customer);
+      } else {
+        res.send('Invalid ID!');
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+});
+
+app.delete('/customer/:id', (req, res) => {
+  Customer.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.send('Customer deleted with sucess');
+    })
+    .catch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+});
+
 app.listen('5555', () => {
   console.log('Up and running - Customer service');
 });
